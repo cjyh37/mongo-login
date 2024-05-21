adc770af0e34979c63d23f0efed825745e27d554
khlee2016 <khlee@seoultech.ac.kr>
2024-05-21 17:30:18 +0900

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
 
 var index = require('./routes/index');
 app.use('/', index);
